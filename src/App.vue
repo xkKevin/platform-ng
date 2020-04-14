1867013e57dd0787b3bffbd45c40000f72ff88b5
sossmc <sossmc@outlook.com>
2020-04-14 10:16:04 +0800

    fix(app.vue): root element id from "#app" to "app"

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,22 @@ export default {
 </script>
 
 <template>
-  <div id="#app">
+  <div id="app">
     {{ msg }}
   </div>
 </template>
 
-<style lang="less">
-div {
-  text-align: center;
+<style>
+body {
+  margin: 0;
+  font-family: 'PingFang SC', -apple-system, BlinkMacSystemFont,
+    'Noto Sans CJK SC', Roboto, 'Helvetica Neue', 'Avenir', Helvetica, Arial,
+    'Hiragino Sans GB', 'Microsoft YaHei', 'Microsoft Jhenghei', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+#app {
+  height: 100vh;
+  width: 100vw;
 }
 </style>
